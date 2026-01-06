01ae00c102124a85298f6c1c97120a56ff26a8d0
ntjennings1 <ntjennings1@gmail.com>
2026-01-05 21:14:02 -0500

    Repo update 0.2

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # Wowlist
+
+A custom wordlist generator.
+
+## Software Requirements
+
+Developing wowlist required the following open-source software packages. W
+
+- figlet 2.2.5-3.1
+- cewl 6.2.1-1
+
+## Usage
+
+This section will cover how to download and run the wowlist generator to your liking.
+
+### Downloading
+
+First, clone the repo:
+```
+git clone https://www.github.com/ntjennings1/wowlist.git
+```
+
+Be sure to give the file execution permissions (with admin privileges):
+```
+sudo chmod 744 $WOWLIST_HOME -R
+```
+
+Next, enter the directory of the wowlist project:
+```
+cd $WOWLIST_HOME
+```
+
+### Running
+
+Once inside the coned directory, users can run the following command to use wowlist features:
+```
+.$WOWLIST_HOME/src/wowlist.sh
+```
+
+### Preferences
+
+This section will cover how to customize program preferences.
+
+
+#### Word Lenngth
+
+Users can customize the length of 
+
+## Acknowledgements
+
+```
+Noah Jennings
+	ntjennings1@gmail.com
+	Old Dominion University
+```
--- src/wowlist.sh
+++ src/wowlist.sh
@@ -1,15 +1,40 @@
 #!/bin/bash
 
-function wow(){
-  sudo cewl -m $2 $1 -w $3 --with-numbers
-}
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+PROJECT_DIR=$(dirname "$SCRIPT_DIR")
+OUT_DIR="${PROJECT_DIR}/out"
+OUT_PATH="${OUT_DIR}/out.txt"
 
-function list(){
-  echo o
+function throw_exec(){
+  if [ "$1" = "params" ]; then
+    echo "[ERR] Requires valid parameters."
+    help main
+  elif [ "$1" = "url" ]; then
+    echo "[ERR] Missing URL."
+    help main
+    exit
+  elif [ "$1" = "len" ]; then
+    echo "[ERR] Missing min word length."
+    help main
+    exit
+  elif [ "$1" = "dep" ]; then
+    echo "[ERR] Missing crawl depth."
+    help main
+    exit
+  elif [ "$1" = "time" ]; then
+    echo "[ERR] Missing crawl duration."
+    help main
+    exit
+  fi
 }
 
 function logo(){
   figlet "WowList"
+}
+
+function wow(){
+  sudo timeout --signal=INT "$4" cewl "$1" -w "$OUT_PATH" -m $2 --with-numbers -d $3
 }
 
 function help(){
@@ -19,26 +44,60 @@ function help(){
     echo "Welcome to the WowList wordlist generation tool."
     echo "Enter the following parameters in order:"
     echo
-    echo "[1] URL"
-    echo "[2] Output file"
-    echo "[3] Min word length"
+    echo "[1] Full URL"
+    echo "[2] Min Word Length"
+    echo "[3] Max Crawl Depth"
+    echo "[4] Run Duration"
     echo
   fi
 
 }
 
-function main(){
-  if [ -n "$1" ]; then
-    logo
-    if [ -n "$2" ]; then
-      if [ -n "$3" ]; then
-        wow $1 $2 $3
-      fi
-    fi
-  else
-    help main
-    echo help
+function diresolve(){
+  echo "[!] Resolving directory structure."
+  if [ ! -d "$OUT_DIR" ]; then
+    echo "[!] Creating output directory."
+    mkdir -p "$OUT_DIR"
   fi
+  echo "[!] Prepping output file."
+  touch "$OUT_PATH"
 }
 
-main $1 $2 $3
+function pkgresolve(){
+  echo "[!] Resolving dependencies."
+  reqs="${PROJECT_DIR}/requirements.txt"
+  while IFS= read -r pkg; do
+      sudo apt-get install "${pkg}" -y > /dev/null
+  done < "$reqs"
+  echo "[!] Requirments satisfied."
+}
+
+function main(){
+#  clear
+  if [ -n "$1" ]; then
+    if [ -n "$2" ]; then
+      if [ -n "$3" ]; then
+	if [ -n "$4" ]; then
+	  diresolve
+	  pkgresolve
+	  logo
+          wow $1 $2 $3 $4
+	else
+	  throw_exec "time"
+	  throw_exec "params"
+	fi
+      else
+        throw_exec "dep"
+        throw_exec "params"
+      fi
+    else
+      throw_exec "len"
+      throw_exec "params"
+    fi
+  else
+    throw_exec "params"
+  fi
+  exit
+}
+
+main $1 $2 $3 $4
